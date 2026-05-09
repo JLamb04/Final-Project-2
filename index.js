85b24ffdcd0da7f20b0e8507fff22e87a55e7c89
JLamb04 <johnnylambiii@gmail.com>
2026-05-09 12:11:04 -0500

    new commit

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+const submitBtn = document.getElementById("submit");
+
+submitBtn.addEventListener("click", function () {
+  submitBtn.classList.add("loading");
+
+  setTimeout(() => {
+    submitBtn.classList.remove("loading");
+  }, 2000);
+});
 
